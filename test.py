3d15d4668a3065677a4ce0ea5fe7be3b016dce99
ikm216 <ikmclassof22@gmail.com>
2024-12-02 19:22:46 -0500

    test:added test to create a document

--- test.py
+++ test.py
@@ -1,21 +1,14 @@
 from pymongo import MongoClient
 import uuid
 
-def test_connection():
+client = MongoClient("mongodb+srv://ikmclassof22:test@cluster0.vtg6m.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0", tlsAllowInvalidCertificates=True)
+db = client['sample_mflix']
+collection = db['users']
 
-    try:
-        client = MongoClient("mongodb+srv://ikmclassof22:test@cluster0.vtg6m.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0", tlsAllowInvalidCertificates=True)
-        db = client['sample_mflix']
-        collection = db.list_collection_names()
+from test import(
+    create_a_document, save_a_document, find_document_uuid, update_document, delete_document
+)
 
-        print("Connection successful!")
-        print("Collections in the database:", collection)
-        
-        return True
-    except Exception as e:
-        print("Connection failed:", e)
-        return False
-
-# Test the connection
-if __name__ == "__main__":
-    test_connection()
+def test_should_return_created_document():
+    uuid_id = create_a_document()
+    assert isinstance(uuid.UUID(uuid_id, version = 4), uuid.UUID)
