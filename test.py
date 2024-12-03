b4354fc8dc6556512966ecaa49c5dbb1f0ec00fd
ikm216 <ikmclassof22@gmail.com>
2024-12-02 19:13:45 -0500

    test: test connection to the client

--- test.py
+++ test.py
@@ -1,4 +1,21 @@
-def test():
-    pass
+from pymongo import MongoClient
+import uuid
 
-test
+def test_connection():
+
+    try:
+        client = MongoClient("mongodb+srv://ikmclassof22:test@cluster0.vtg6m.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0", tlsAllowInvalidCertificates=True)
+        db = client['sample_mflix']
+        collection = db.list_collection_names()
+
+        print("Connection successful!")
+        print("Collections in the database:", collection)
+        
+        return True
+    except Exception as e:
+        print("Connection failed:", e)
+        return False
+
+# Test the connection
+if __name__ == "__main__":
+    test_connection()
