db39378cfe135a37d65e835018f8b4bc2432bcb9
ikm216 <ikmclassof22@gmail.com>
2024-12-02 19:34:28 -0500

    feat:added feat to create a document

--- test.py
+++ test.py
@@ -5,9 +5,14 @@ client = MongoClient("mongodb+srv://ikmclassof22:test@cluster0.vtg6m.mongodb.net
 db = client['sample_mflix']
 collection = db['users']
 
-from test import(
-    create_a_document, save_a_document, find_document_uuid, update_document, delete_document
-)
+def create_a_document():
+    document = {
+        "UUID": str(uuid.uuid4()),
+        "name": "John Doe",
+        "email": "john.doe@example.com",
+        "age": 30
+    }
+    return document["UUID"], document
 
 def test_should_return_created_document():
     uuid_id, document = create_a_document()
