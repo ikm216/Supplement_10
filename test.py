95b96b7b63865580776a8e714bdc5f2077090354
ikm216 <ikmclassof22@gmail.com>
2024-12-02 19:39:50 -0500

    test:add the client and database to test for creating a document

--- test.py
+++ test.py
@@ -6,6 +6,12 @@ db = client['sample_mflix']
 collection = db['users']
 
 def create_a_document():
+    """
+    Creates a random MongoDB document and returns its UUID.
+
+    Returns:
+        The UUID of the document and the document.
+    """
     document = {
         "UUID": str(uuid.uuid4()),
         "name": "John Doe",
@@ -15,6 +21,9 @@ def create_a_document():
     return document["UUID"], document
 
 def test_should_return_created_document():
+    client = MongoClient("mongodb+srv://ikmclassof22:test@cluster0.vtg6m.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0", tlsAllowInvalidCertificates=True)
+    db = client['sample_mflix']
+    collection = db['users']
     uuid_id, document = create_a_document()
     assert isinstance(uuid.UUID(uuid_id, version = 4), uuid.UUID)
     assert document["UUID"] == uuid_id
@@ -23,3 +32,5 @@ def test_should_return_created_document():
     assert "age" in document
     assert isinstance(uuid_id, str)
     assert len(uuid_id) == 36
+
+
